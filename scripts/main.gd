56f920da417334ecd0844deb10698b7ae0961cd7
Pac4games <paulo.nicolau7@gmail.com>
2024-11-08 11:05:22 +0000

    Finished basic Flappy Bird mechanics

--- scripts/main.gd
+++ scripts/main.gd
@@ -3,6 +3,8 @@ extends Node
 @onready var player:CharacterBody2D = $Fixe
 @onready var ground:Area2D = $Ground
 @onready var timer:Timer = $PipeTimer
+@onready var score_label:Label = $ScoreLabel
+@onready var restart_button:CanvasLayer = $GameOver
 
 @export var pipe_scene:PackedScene
 @export var SCROLL_SPEED:int = 4
@@ -20,8 +22,11 @@ var screen_size:Vector2i
 func new_game() -> void:
 	game_running = false
 	game_over = false
-	score = 0
 	scroll = 0
+	score = 0
+	score_label.text = str(score)
+	restart_button.hide()
+	get_tree().call_group("pipes", "queue_free")
 	pipes.clear()
 	generate_pipes()
 	player.reset()
@@ -32,19 +37,45 @@ func start_game() -> void:
 	player.flop()
 	timer.start()
 
+func stop_game():
+	timer.stop()
+	player.flying = false
+	game_running = false
+	game_over = true
+	restart_button.show()
+
 func player_hit() -> void:
-	pass
+	player.falling = true
+	stop_game()
+
+func player_scored() -> void:
+	if (!player.falling):
+		score += 1
+		score_label.text = str(score)
 
 func generate_pipes() -> void:
 	var pipe:Node2D = pipe_scene.instantiate()
 	pipe.position.x = screen_size.x + PIPE_DELAY
 	pipe.position.y = (float(screen_size.y - ground_height) / 2 ) + randi_range(-PIPE_RANGE, PIPE_RANGE) 
 	pipe.hit.connect(player_hit)
+	pipe.scored.connect(player_scored)
 	add_child(pipe)
 	pipes.append(pipe)
 
+func check_top():
+	if (player.position.y < 0):
+		player.falling = true
+		stop_game()
+
 func _on_pipe_timer_timeout() -> void:
 	generate_pipes()
+
+func _on_ground_hit() -> void:
+	player.falling = false
+	stop_game()
+
+func _on_game_over_restart() -> void:
+	new_game()
 
 func _ready() -> void:
 	screen_size = get_window().size
@@ -58,8 +89,9 @@ func _physics_process(_delta:float) -> void:
 				start_game()
 			elif player.flying:
 				player.flop()
+				check_top()
 
-func _process(_delta:float) -> void:
+func _process(delta:float) -> void:
 	if (game_running):
 		scroll += SCROLL_SPEED
 		# Reset ground scroll
